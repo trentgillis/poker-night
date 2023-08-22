7298836fd6c0ac17e0b03ae8e918fc76cccf742a
Trentin Gillis <trentin.gillis@gmail.com>
2023-08-22 10:16:33 -0600

    update fields on account edit page to be disabled unless editing

--- app/javascript/controllers/user_registration_edit_controller.js
+++ app/javascript/controllers/user_registration_edit_controller.js
@@ -2,7 +2,7 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
   static values = { isEditable: { type: Boolean } };
-  static targets = ['saveBtn', 'editBtn', 'cancelBtn'];
+  static targets = ['saveBtn', 'editBtn', 'cancelBtn', 'emailInput', 'firstNameInput', 'lastNameInput'];
 
   onConnect() {
     this.isEditableValue = false;
@@ -12,14 +12,34 @@ export default class extends Controller {
     this.isEditableValue = !this.isEditableValue;
 
     if (this.isEditableValue) {
+      this.enabledFormFields();
       this.showSaveBtn();
       this.showCancelBtn();
       this.hideEditBtn();
     } else {
+      this.disableFormFields();
       this.hideSaveBtn();
       this.hideCancelBtn();
       this.showEditBtn();
     }
+  }
+
+  enabledFormFields() {
+    this.emailInputTarget.classList.remove('input-text-disabled');
+    this.emailInputTarget.disabled = false;
+    this.firstNameInputTarget.classList.remove('input-text-disabled');
+    this.firstNameInputTarget.disabled = false;
+    this.lastNameInputTarget.classList.remove('input-text-disabled');
+    this.lastNameInputTarget.disabled = false;
+  }
+
+  disableFormFields() {
+    this.emailInputTarget.classList.add('input-text-disabled');
+    this.emailInputTarget.disabled = true;
+    this.firstNameInputTarget.classList.add('input-text-disabled');
+    this.firstNameInputTarget.disabled = true;
+    this.lastNameInputTarget.classList.add('input-text-disabled');
+    this.lastNameInputTarget.disabled = true;
   }
 
   hideSaveBtn() {
