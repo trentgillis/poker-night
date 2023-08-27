19c96aeecd3ff392a29690bfabe0f95371887251
Trentin Gillis <trentin.gillis@gmail.com>
2023-08-27 15:09:49 -0600

    automatically hide toast after 5 seconds

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -3,6 +3,12 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static targets = ['toast'];
 
+  connect() {
+    setTimeout(() => {
+      this.hideToast();
+    }, 5000);
+  }
+
   hideToast() {
     this.toastTarget.remove();
   }
