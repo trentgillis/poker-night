012d5e912a1eb308452813166b2c6984383d3629
Trentin Gillis <trentin.gillis@gmail.com>
2023-08-28 17:28:29 -0600

    add slide transition to flash toast

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -1,15 +1,23 @@
 import { Controller } from '@hotwired/stimulus';
+import { useTransition } from 'stimulus-use';
 
 export default class extends Controller {
   static targets = ['toast'];
 
   connect() {
+    useTransition(this, {
+      element: this.toastTarget,
+    });
+
     setTimeout(() => {
       this.hideToast();
     }, 5000);
+
+    // Runs the enter transition
+    this.enter();
   }
 
   hideToast() {
-    this.toastTarget.remove();
+    this.leave();
   }
 }
