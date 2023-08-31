5cc5e42b59275f5bdd7925cf0e8276cbdfa4326a
Trentin Gillis <trentin.gillis@gmail.com>
2023-08-31 08:09:16 -0600

    add validations to results

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,6 +1,9 @@
 class Result < ApplicationRecord
   belongs_to :user
 
+  validates :buy_in_amount_cents, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :win_amount_cents, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
   monetize :buy_in_amount_cents, as: :buy_in_amount
   monetize :win_amount_cents, as: :win_amount
 end
