eed419a1d8683d012d14c03b45d6cd1c0b0f23ab
Trentin Gillis <trentin.gillis@gmail.com>
2023-09-02 08:02:16 -0600

    add log to calculate users total winnings to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   has_many :results
+
+  def total_winnings
+    results.map do |result|
+      result.win_amount_cents - result.buy_in_amount_cents
+    end.sum
+  end
 end
