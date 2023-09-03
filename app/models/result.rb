30364dae8ddad1c688ac831dfe66158797120320
Trentin Gillis <trentin.gillis@gmail.com>
2023-09-03 14:34:24 -0600

    only allows vlaues less than  for buy in amount and win amount

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,9 +1,9 @@
 class Result < ApplicationRecord
   belongs_to :user
 
-  validates :buy_in_amount_cents, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :win_amount_cents, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :buy_in_amount_cents, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10_000 }
+  validates :win_amount_cents, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10_000 }
 
-  monetize :buy_in_amount_cents, as: :buy_in_amount
-  monetize :win_amount_cents, as: :win_amount
+  monetize :buy_in_amount_cents, as: :buy_in_amount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  monetize :win_amount_cents, as: :win_amount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
