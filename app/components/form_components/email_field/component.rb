16b0de0df76d565a14b94e58071697e37bb0a39f
Trentin Gillis <trentin.gillis@gmail.com>
2023-09-09 10:49:05 -0600

    use generic input base component has parent for email field component

--- app/components/form_components/email_field/component.rb
+++ app/components/form_components/email_field/component.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 module FormComponents
-  class EmailField::Component < ApplicationViewComponent
-    def initialize(form:, input_name:, input_data: {}, errors: [], disabled: false)
-      @form = form
-      @input_name = input_name
-      @input_data = input_data
-      @errors = errors
-      @disabled = disabled
+  class EmailField::Component < InputBaseComponent
+    def initialize(args)
+      super(**args)
     end
   end
 end
