03d2f13ae760821d05a3916ea9091c909a6db6b1
Trentin Gillis <trentin.gillis@gmail.com>
2023-09-09 10:15:42 -0600

    componentize password field

---
+++ app/components/PasswordField/component.rb
@@ -0,0 +1,7 @@
+class PasswordField::Component < ApplicationViewComponent
+  def initialize(form:, input_name:, label_text: nil)
+    @form = form
+    @input_name = input_name
+    @label_text = label_text
+  end
+end
