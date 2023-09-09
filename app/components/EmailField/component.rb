3cc53b8e9e52c81d8d5d446e5d49a3abc1a37d37
Trentin Gillis <trentin.gillis@gmail.com>
2023-09-08 19:21:50 -0600

    componentize email input fields

---
+++ app/components/EmailField/component.rb
@@ -0,0 +1,9 @@
+class EmailField::Component < ApplicationViewComponent
+  def initialize(form:, input_name:, input_data: {}, errors: [], disabled: false)
+    @form = form
+    @input_name = input_name
+    @input_data = input_data
+    @errors = errors
+    @disabled = disabled
+  end
+end
