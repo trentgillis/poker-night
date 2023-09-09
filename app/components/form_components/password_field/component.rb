a03b6792a21e0242b37208b4197e04d8756d5d3a
Trentin Gillis <trentin.gillis@gmail.com>
2023-09-09 11:00:16 -0600

    use input base as parent for password input component

--- app/components/form_components/password_field/component.rb
+++ app/components/form_components/password_field/component.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 module FormComponents
-  class PasswordField::Component < ApplicationViewComponent
-    def initialize(form:, input_name:, label_text: nil)
-      @form = form
-      @input_name = input_name
-      @label_text = label_text
+  class PasswordField::Component < InputBaseComponent
+    def initialize(args)
+      super(**args)
     end
   end
 end
