38867f73d5b557e5ae43b0032c771399f9eed49e
Trentin Gillis <trentin.gillis@gmail.com>
2023-09-09 12:15:23 -0600

    add pretty form validation to new user registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
 
   has_many :results
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def total_winnings
     results.map do |result|
       result.win_amount - result.buy_in_amount
