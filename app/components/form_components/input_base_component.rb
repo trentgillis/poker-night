15aad841024c7907eba33f00ed4037d159a4ee4c
Trentin Gillis <trentin.gillis@gmail.com>
2023-09-09 12:41:20 -0600

    remove unneeded disabled prop from input view components

--- app/components/form_components/input_base_component.rb
+++ app/components/form_components/input_base_component.rb
@@ -1,11 +1,10 @@
 module FormComponents
   class InputBaseComponent < ApplicationViewComponent
-    def initialize(form:, input_name:, errors: [], label_text: nil, disabled: false, input_data_attrs: {})
+    def initialize(form:, input_name:, errors: [], label_text: nil, input_data_attrs: {})
       @form = form
       @input_name = input_name
       @errors = errors
       @label_text = label_text
-      @disabled = disabled
       @input_data_attrs = input_data_attrs
     end
   end
