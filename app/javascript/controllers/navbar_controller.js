ae78d2204e8550d1f8e5bf0152104fda429011b5
Trentin Gillis <trentin.gillis@gmail.com>
2023-10-24 22:36:01 -0600

    update navbar user menu to use el-transition

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,32 +1,19 @@
 import { Controller } from '@hotwired/stimulus';
-import { useClickOutside, useTransition } from 'stimulus-use';
+import { useClickOutside } from 'stimulus-use';
+import { leave, toggle } from 'el-transition';
 
 export default class extends Controller {
-  static values = { userMenuOpen: Boolean };
   static targets = ['userMenuBtn', 'userMenu'];
 
   connect() {
     useClickOutside(this);
-    useTransition(this, {
-      element: this.userMenuTarget,
-      transitioned: false,
-    });
   }
 
   toggle() {
-    this.userMenuOpenValue = !this.userMenuOpenValue;
-
-    if (this.userMenuOpenValue) {
-      this.enter();
-    } else {
-      this.leave();
-    }
+    toggle(this.userMenuTarget);
   }
 
   clickOutside(event) {
-    if (this.userMenuOpenValue) {
-      this.userMenuOpenValue = false;
-      this.leave();
-    }
+    leave(this.userMenuTarget);
   }
 }
