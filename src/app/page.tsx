e4a0d102f18ff94ec486b609aa2d90b68590bdb2
Trentin Gillis <trentin.gillis@gmail.com>
2025-01-11 17:26:05 -0700

    initial laravel commit

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { trpc } from "@/trpc/server";
-
-export default async function Home() {
-  const greeting = await trpc.hello.hello({ name: "Trent" });
-
-  return <div>{greeting.hello}</div>;
-}
--- src/server/api/root.ts
+++
@@ -1,9 +0,0 @@
-import { createTRPCRouter } from "@/server/api/trpc";
-
-import { helloRouter } from "./routers/hello";
-
-export const appRouter = createTRPCRouter({
-  hello: helloRouter,
-});
-
-export type AppRouter = typeof appRouter;
