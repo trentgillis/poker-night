b3529696262fa089540ad5c79e500c2ab8401d00
Trentin Gillis <trentin.gillis@gmail.com>
2025-01-20 08:36:23 -0700

    nav styling tweaks

--- resources/js/Components/Nav/Nav.tsx
+++ resources/js/Components/Nav/Nav.tsx
@@ -5,18 +5,18 @@ import { Menu } from 'lucide-react';
 
 export default function Nav() {
   return (
-    <nav className="flex h-16 w-full items-center border-b border-zinc-400">
+    <nav className="flex h-14 w-full items-center border-b border-zinc-400">
       <div className="w-full flex-1">
         <button
           type="button"
           className="flex h-12 w-12 items-center justify-center"
         >
-          <Menu strokeWidth={1} />
+          <Menu size={20} strokeWidth={1} />
         </button>
       </div>
       <div className="flex flex-1 justify-center">
         <Link href="/">
-          <Logo height={24} />
+          <Logo height={20} />
         </Link>
       </div>
       <div className="flex-1"></div>
