51bf3466aa2fdfd8a3b799fc64cddd9b7dd6b87c
Trentin Gillis <trentin.gillis@gmail.com>
2025-01-20 18:17:05 -0700

    create nav link component and render correct nav items in nav menu

---
+++ resources/js/Components/Nav/NavLink.tsx
@@ -0,0 +1,23 @@
+import { Link, usePage } from '@inertiajs/react';
+import React from 'react';
+
+interface NavLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+export default function NavLink({ href, children }: NavLinkProps) {
+  const { url } = usePage();
+
+  return (
+    <li>
+      {/*Current: "bg-indigo-700 text-white", Default: "text-indigo-200 hover:text-white hover:bg-indigo-700"*/}
+      <Link
+        href={href}
+        className={`flex items-center rounded ${url === href ? 'bg-zinc-800' : 'bg-zinc-950'} px-3 py-2 text-sm font-medium text-zinc-100`}
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}
