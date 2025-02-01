db5e35ca04e4be1ef357ab6ff69f8232a7dbdf25
Trentin Gillis <trentin.gillis@gmail.com>
2025-02-01 12:24:48 -0700

    create CashGame model and migration

---
+++ database/migrations/2025_02_01_192119_create_cash_games_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cash_games', function (Blueprint $table) {
+            $table->id();
+            $table->string('stakes');
+            $table->date('date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cash_games');
+    }
+};
