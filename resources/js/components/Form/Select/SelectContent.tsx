f12c63e3277203c6d31fa6f01e6309ab1105c596
Trentin Gillis <trentin.gillis@gmail.com>
2025-02-04 20:37:41 -0700

    cleanup select content component

--- resources/js/components/Form/Select/SelectContent.tsx
+++ resources/js/components/Form/Select/SelectContent.tsx
@@ -6,7 +6,6 @@ export default function SelectContent({
   className,
   ref,
   children,
-  position = 'popper',
   ...props
 }: React.ComponentPropsWithRef<typeof SelectPrimitive.Content>) {
   return (
@@ -15,19 +14,15 @@ export default function SelectContent({
         ref={ref}
         className={twMerge(
           'bg-background text-white-muted data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 border-border relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border shadow-md',
-          position === 'popper' &&
-            'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
+          'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
           className,
         )}
-        position={position}
+        position="popper"
         {...props}
       >
         <SelectPrimitive.Viewport
           className={twMerge(
-            'p-1',
-            position === 'popper'
-              ? 'h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]'
-              : '',
+            'h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] p-1',
           )}
         >
           {children}
