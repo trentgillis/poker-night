a1ddedb09873adb86e70280b6d71cc5dd2a27acd
Trentin Gillis <trentin.gillis@gmail.com>
2025-02-08 11:09:35 -0700

    minor style updates to the in progress game banner

--- resources/js/components/Layout/InProgressGameBanner.tsx
+++ resources/js/components/Layout/InProgressGameBanner.tsx
@@ -11,12 +11,12 @@ export default function InProgressGameBanner() {
       onClick={handleClick}
       className="border-border flex w-full items-center rounded-md border px-3 py-4"
     >
-      <div className="flex flex-auto flex-col items-start gap-0.5">
-        <div className="text-sm">Game currently in progress</div>
+      <div className="flex flex-auto flex-col items-start gap-1">
+        <div className="text-sm font-semibold">Game currently in progress</div>
         <div className="text-white-muted text-xs">5/10 No Limit Hold'em</div>
       </div>
       <div className="flex items-center justify-center gap-0.5">
-        <span className="text-xs font-semibold">Join Game</span>
+        <span className="text-xs font-medium">Join Game</span>
         <ChevronsRight className="h-4 w-4" />
       </div>
     </button>
