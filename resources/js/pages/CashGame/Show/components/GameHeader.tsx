9ab211358d0ae1df0fcea73ef91ae017d62e349e
Trentin Gillis <trentin.gillis@gmail.com>
2025-02-08 12:51:27 -0700

    fix game header tz

--- resources/js/pages/CashGame/Show/components/GameHeader.tsx
+++ resources/js/pages/CashGame/Show/components/GameHeader.tsx
@@ -25,6 +25,7 @@ export default function GameHeader({
             month: 'short',
             day: 'numeric',
             year: 'numeric',
+            timeZone: 'utc',
           })}
         </span>
         {gameStatus === 'in_progress' && (
