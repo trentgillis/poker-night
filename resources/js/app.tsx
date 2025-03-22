b83b5e6bbb08198729ee8f5ade14853800ba3892
Trent Gillis <trentin.gillis@gmail.com>
2025-03-22 21:07:52 +0000

    updates for lab deployment

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,7 +5,7 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
-const appName = import.meta.env.VITE_APP_NAME || 'PokerNight';
+const appName = 'PokerNight';
 
 createInertiaApp({
   title: (title) => `${appName}`,
